bb2655db01f63b9453a027e59cfc0b5cf62292e2
Timo Poppinga <timo.poppinga@zdrei.com>
2023-11-02 20:50:22 +0100

    fix stan

--- src-contracts/StructCollectionInterface.php
+++ src-contracts/StructCollectionInterface.php
@@ -10,4 +10,5 @@ interface StructCollectionInterface extends \Countable, \Iterator
      * @return array<StructInterface>
      */
     public function getValues(): array;
+    public function addValue(StructInterface $struct): void;
 }
